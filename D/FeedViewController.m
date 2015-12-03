4cd66bc4ddc43c8ae70b9acf4e713b070e3a86a1
Emma Nimstad <emma@nimstad.net>
2015-12-03 18:28:09 +0100

    Initial commit (?)

--- D/FeedViewController.m
+++ D/FeedViewController.m
@@ -41,7 +41,9 @@ NSString *feedCellID = @"sbfeedcell";               // UICollectionViewCell stor
     cell.text.text = post.text;
     cell.date.text = post.date;
     
-    [cell setFrame:CGRectMake(cell.frame.origin.x, cell.frame.origin.y, [self.win], cell.frame.size.height)];
+    [cell setFrame:CGRectMake(cell.frame.origin.x, cell.frame.origin.y, self.view.window.frame.size.width, cell.frame.size.height)];
+//    cell.center.x = self.view.window.center.x;
+//    [cell.bo]
     
     return cell;
 }
