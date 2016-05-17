42aa65834e90257a3a996600500a384322b35bf0
Emma Nimstad <emma@nimstad.net>
2016-05-17 21:18:35 +0200

    Pretty line under cells

--- D/FeedTableController.m
+++ D/FeedTableController.m
@@ -39,15 +39,20 @@ NSString *reusableCellId = @"tablefeedcell";
     cell.text.text = post.text;
     cell.date.text = post.date;
     
-//    [cell setSeparatorInset:<#(UIEdgeInsets)#>]
+    // Remove seperator inset
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+        [cell setSeparatorInset:UIEdgeInsetsMake(0, 12, 0, 12)];
+    }
     
-//    [cell setFrame:CGRectMake(0, cell.frame.origin.y, self.view.window.bounds.size.width, cell.frame.size.height)];
+    // Prevent the cell from inheriting the Table View's margin settings
+    if ([cell respondsToSelector:@selector(setPreservesSuperviewLayoutMargins:)]) {
+        [cell setPreservesSuperviewLayoutMargins:NO];
+    }
     
-    //  Adds a shadow under the cell
-//    cell.layer.masksToBounds = NO;
-//    cell.layer.shadowOffset = CGSizeMake(0, 8);
-//    cell.layer.shadowRadius = 3;
-//    cell.layer.shadowOpacity = 0.2;
+    // Explictly set your cell's layout margins
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+        [cell setLayoutMargins:UIEdgeInsetsZero];
+    }
     
     return cell;
 }
