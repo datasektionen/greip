78d073b5007eef9e2437a7314fa54c09b8b13b9e
Emma Nimstad <emma@nimstad.net>
2020-01-16 23:40:24 +0100

    Add meta data to expanded cells.

--- Greip/FeedCell.swift
+++ Greip/FeedCell.swift
@@ -13,7 +13,18 @@ import UIKit
 class FeedCell : UITableViewCell {
     @IBOutlet fileprivate var content: UILabel!
     @IBOutlet fileprivate var title: UILabel!
+	@IBOutlet fileprivate var meta: UILabel!
+	@IBOutlet fileprivate var metaTopConstraint: NSLayoutConstraint!
+	@IBOutlet fileprivate var metaBottomConstraint: NSLayoutConstraint!
+	@IBOutlet fileprivate var contentBottomConstraint: NSLayoutConstraint!
 	var expanded: Bool = false
+	var post: Post! {
+		didSet {
+			content.text = post.content.string
+			title.text = post.title
+			meta.text = "\(post.author), \(post.date)"
+		}
+	}
 
 	override func awakeFromNib() {
 		super.awakeFromNib()
@@ -23,6 +34,7 @@ class FeedCell : UITableViewCell {
 
 	override func prepareForReuse() {
 		self.expanded = false
+		self.meta.isHidden = true
 		self.fixExpanded()
 	}
 	
@@ -33,13 +45,9 @@ class FeedCell : UITableViewCell {
 
 	func fixExpanded() {
 		self.content.numberOfLines = self.expanded ? 0 : 3
+		self.meta.isHidden = !self.expanded
+		self.metaTopConstraint.isActive = self.expanded
+		self.metaBottomConstraint.isActive = self.expanded
+		self.contentBottomConstraint.isActive = !self.expanded
 	}
-
-	var post: Post! {
-		didSet {
-			content.text = post.content.string
-			title.text = post.title
-		}
-	}
-    
 }
